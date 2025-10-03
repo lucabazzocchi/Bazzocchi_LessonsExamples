65a2a3dc734200be693ad4476aeca0da685bcd9d
Luca Bazzocchi <LUCA.BAZZOCCHI@local.ittcesena.it>
2025-10-03 09:55:33 +0200

    try

---
+++ src/BlaisePascal.LessonsExamples.Domain/Enemy.cs
@@ -0,0 +1,58 @@
+﻿namespace BlaisePascal.LessonsExamples.Domain
+{
+    public class Enemy
+    {
+        //TODO: Attributes decomposition
+        // Attributes
+        private string _name;
+        private string _description;
+        private int _health;
+        private bool _isAlive;
+     
+
+        // Constructor
+
+        //OverLoading costruttore
+        public Enemy()
+        {
+
+        }
+
+        public Enemy(string name)
+        {
+            SetName(name);
+        }
+
+        public Enemy(string name, int health)
+        {
+            SetName(name);
+            SetHealth(health);
+
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+        public int GetHealth()
+        {
+            return _health;
+        }
+
+        // Function
+        public void SetName(string newName)
+        {
+            if (!string.IsNullOrWhiteSpace(newName))
+                _name = newName;
+        }
+
+        public void SetHealth(int newHealth)
+        {
+            if (int.IsPositive(newHealth) && newHealth <= 100)
+                _health = newHealth;
+        }
+
+
+
+    }
+}
---
+++ test/BlaisePascal.LessonsExamples.Domain.UnitTests/EnemyTest.cs
@@ -0,0 +1,33 @@
+﻿namespace BlaisePascal.LessonsExamples.Domain.UnitTests
+{
+    public class EnemyTest
+    {
+        [Fact]
+        public void EnemyName_WhenNameIsValid_TheNameMustBeAssignedCorrectly()
+        {
+            // Arrange
+            //primo costruttore senza parametri
+            Enemy newEnemy = new Enemy();
+
+            //Act
+            newEnemy.SetName("Stefano");
+
+            //Assert
+            Assert.Equal("Stefano", newEnemy.GetName());
+        
+        }
+
+        [Fact]
+        public void EnemyName_TheNameCannotBeNull()
+        {
+            //Arrange
+            Enemy newEnemy = new Enemy();
+
+            //Act
+            newEnemy.SetName(null);
+            Assert.Null(newEnemy.GetName());
+        }
+            
+    }
+            
+}
